7a6bfeaff1ffc753ff5c5b66517ee19723b4072e
PortatilCurroSnake <michael.retamozo@gmail.com>
2021-11-19 20:38:53 +0100

    a

---
+++ JS-MODERNO/script/17-clases.js
@@ -0,0 +1,21 @@
+// Molde
+class Coche{
+
+    constructor(){
+        // Propiedades o atributos
+        this.marca = "Mercedes";
+        this.modelo = "Clase A";
+        this.color = "Negro";
+        this.velocidad = 0;
+        this.year = 2020;
+    }
+    
+}
+
+// Crear objeto
+
+let mi_coche = new Coche();
+
+console.log(mi_coche.marca, mi_coche.modelo);
+
+let segundo_coche = new Coche();
